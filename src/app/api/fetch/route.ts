88abfc2dbd9c086500215439277f16f8e624c6c9
wodwiii laptop <rodrigo.barien.iii@gmail.com>
2024-07-29 13:22:52 +0800

    fixed data revalidation

--- src/app/api/fetch/route.ts
+++ src/app/api/fetch/route.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
-
+export const revalidate = 0;
 export async function GET(request: Request) {
   const activities = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 15; i++) {
     const res = await axios.get("https://bored-api.appbrewery.com/random");
     activities.push(res.data);
   }
